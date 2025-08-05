11323193e19932b16d61da8aa23f9c2e56cbc7bb
Shred <frederikstadler@gmail.com>
2025-08-05 17:59:44 +0200

    test

--- HighlightMapEnchantsSettings.cs
+++ HighlightMapEnchantsSettings.cs
@@ -10,45 +10,18 @@ namespace HighlightMapEnchants;
 public class HighlightMapEnchantsSettings : ISettings
 {
     //Mandatory setting to allow enabling/disabling your plugin
-    public ToggleNode Enable { get; set; } = new(false);
+    public ToggleNode Enable { get; set; } = new ToggleNode(false);
 
-    public ToggleNode ShowBreachedMapEnchant { get; set; } = new(true);
-    public ColorNode BreachedMapHighlightColor { get; set; } = new(Color.Purple);
+    private static Color essenceColor = new Color(170, 91, 222); 
+    private static Color harvestColor = new Color(51, 153, 255);
+    private static Color beastColor = new Color(255, 250, 0);
+    public ToggleNode ShowBeastEnchant { get; set; } = new(true);
+    public ColorNode BeastHighlightColor { get; set; } = new ColorNode(beastColor);
+    public ToggleNode ShowHarvestEnchant { get; set; } = new(true);
+    public ColorNode HarvestHighlightColor { get; set; } = new ColorNode(harvestColor);
+    public ToggleNode ShowCrystalPrisonEnchant { get; set; } = new(true);
+    public ColorNode EssenceHighlightColor { get; set; } = new ColorNode(essenceColor);
 
-    public ToggleNode ShowDemonAbyssEnchant { get; set; } = new(true);
-    public ColorNode DemonAbyssHighlightColor { get; set; } = new(Color.Red);
-
-    public ToggleNode ShowEssenceExilesEnchant { get; set; } = new(true);
-    public ColorNode EssenceExilesHighlightColor { get; set; } = new(Color.Lavender);
-
-    public ToggleNode ShowFlashBreachesEnchant { get; set; } = new(true);
-    public ColorNode FlashBreachesHighlightColor { get; set; } = new(Color.MediumPurple);
-
-    public ToggleNode ShowHarbingerPortalsEnchant { get; set; } = new(true);
-    public ColorNode HarbingerPortalsHighlightColor { get; set; } = new(Color.LightSkyBlue);
-
-    public ToggleNode ShowHarvestBeastsEnchant { get; set; } = new(true);
-    public ColorNode HarvestBeastsHighlightColor { get; set; } = new(Color.Blue);
-
-    public ToggleNode ShowHugeHarvestEnchant { get; set; } = new(true);
-    public ColorNode HugeHarvestHighlightColor { get; set; } = new(Color.DarkBlue);
-
-    public ToggleNode ShowPantheonShrinesEnchant { get; set; } = new(true);
-    public ColorNode PantheonShrinesHighlightColor { get; set; } = new(Color.Yellow);
-
-    public ToggleNode ShowPlayerIsHarbingerEnchant { get; set; } = new(true);
-    public ColorNode PlayerIsHarbingerHighlightColor { get; set; } = new(Color.White);
-
-    public ToggleNode ShowReverseIncursionEnchant { get; set; } = new(true);
-    public ColorNode ReverseIncursionHighlightColor { get; set; } = new(Color.DeepPink);
-
-    public ToggleNode ShowStrongboxChainEnchant { get; set; } = new(true);
-    public ColorNode StrongboxChainHighlightColor { get; set; } = new(Color.LightYellow);
-
-    public ToggleNode ShowTormentedMonstersEnchant { get; set; } = new(true);
-    public ColorNode TormentedMonstersHighlightColor { get; set; } = new(Color.Green);
-
-    
     #region Auto-Roll
     public ToggleNode EnableAutoRoll { get; set; } = new(false);
 
